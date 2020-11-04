28a3c946b29d24f975f570bb818a424c5c38a44a
rr- <rr-@sakuya.pl>
2020-11-04 10:15:58 +0100

    build: 2.3.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="rr-",
     author_email="rr-@sakuya.pl",
     name="ass_tag_parser",
-    version="2.3",
+    version="2.3.1",
     description="Parser of ASS tags",
     long_description=(Path(__file__).parent / "README.md").read_text(
         encoding="utf-8"
