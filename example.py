97a739a8385b1d92ffc4dd1ed343761ebe2e403b
rr- <rr-@sakuya.pl>
2021-08-04 23:52:30 +0200

    misc: update example

--- example.py
+++ example.py
@@ -1,13 +1,15 @@
 from ass_tag_parser import parse_ass, parse_draw_commands
 
-result = parse_ass(
+result1 = parse_ass(
     r"{\an5\pos(175,460)\fnUtopia with Oldstyle figures\fs90\bord0\blur3"
     r"\1c&H131313&\t(0,1000,2,\1c&H131340&)\t(1000,2000,\1c&H1015B2&"
     r"\blur1.4)}Attack No. 1{NOTE:アタックNo.1}"
 )
-print(result)
-print(result[2].meta)
+print(result1)
+print(result1[2].meta)
 
-result = parse_draw_commands(r"m 50 0 b 100 0 100 100 50 100 b 0 100 0 0 50 0")
-print(result)
-print(result[1].meta)
+result2 = parse_draw_commands(
+    r"m 50 0 b 100 0 100 100 50 100 b 0 100 0 0 50 0"
+)
+print(result2)
+print(result2[1].meta)
